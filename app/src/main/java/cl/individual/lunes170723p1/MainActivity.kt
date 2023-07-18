32c6586813b4f89b8099c53901566fc9cbc41c40
Emilia <correoalcantaraemilia@gmail.com>
2023-07-17 21:17:42 -0400

    Activación de view binding

--- app/src/main/java/cl/individual/lunes170723p1/MainActivity.kt
+++ app/src/main/java/cl/individual/lunes170723p1/MainActivity.kt
@@ -2,10 +2,14 @@ package cl.individual.lunes170723p1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cl.individual.lunes170723p1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
